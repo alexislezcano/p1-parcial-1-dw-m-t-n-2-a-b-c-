3c3d8d0ad21207cb6fe51c23dd37a4dfc87a8a25
alexislezcano <alexislezcano79@gmail.com>
2022-11-06 22:12:58 -0300

    Últimos cambios realizados

--- p1-parcial-1-master/index.js
+++ p1-parcial-1-master/index.js
@@ -4,39 +4,6 @@
  * APELLIDO, NOMBRE | APELLIDO, NOMBRE
  */
 
-// Ejemplo de la estructura de un disco:
-// let disco = {
-//     Nombre: 'El lado oscuro de la Programación',
-//     Autor: 'Los Programadores Anónimos',
-//     Codigo: 1,
-//     Pistas: [
-//         {
-//             Nombre: 'Esa cajita loca llamada variablecita',
-//             Duracion: 200,
-//         },
-//         {
-//             Nombre: 'Nunca quise ser un NaN',
-//             Duracion: 180,
-//         },
-//         {
-//             Nombre: 'No quiero programar',
-//             Duracion: 90,
-//         },
-//         {
-//             Nombre: 'Bajo presión',
-//             Duracion: 240,
-//         },
-//         {
-//             Nombre: 'La odisea de las variables privadas',
-//             Duracion: 120,
-//         },
-//         {
-//             Nombre: 'Sr. Programador',
-//             Duracion: 720,
-//         },
-//     ],
-// };
-
 // Discos:
 
 let discos = []
@@ -48,20 +15,6 @@ let nombrePista;
 let duracionPista;
 let pista;
 
-
-
-
-//Esto es un harcodeo
-
-for(let i = 0; i < 4; i++){
-    disco = {
-    Nombre: "Fulano",
-    Autor: "Fulana",
-    Codigo: i,
-    Pistas:[]
-    }
-    discos.push(disco)
-}
 
 function agregar() {
     nombreDisco = validarTextoVacio(" del disco");
@@ -96,11 +49,11 @@ function agregar() {
 
 function validarCodigo(discoVerificar){
 
-    let numeroIgresado = Number(prompt("Ingrese el número de código:"))
+    let numeroIgresado = parseInt(prompt("Ingrese el número de código:"))
     for (let i = 0; i < discoVerificar.length; i++){
         while((numeroIgresado == discoVerificar[i].Codigo) || (numeroIgresado < 1 || numeroIgresado > 999)){
             alert("codigo ya existente")
-            numeroIgresado = Number(prompt("Ingrese el número de código "));
+            numeroIgresado = parseInt(prompt("Ingrese el número de código "));
         }
     }
     return numeroIgresado;
@@ -110,11 +63,11 @@ function validarCodigo(discoVerificar){
 //La duración de cada pista debe estar entre 0 y 7200 (segundos) inclusive.
 function validarDuracion(duracionVerificar){
 
-    let numeroIgresado = Number(prompt("Ingrese la duración de la pista:"))
+    let numeroIgresado = parseInt(prompt("Ingrese la duración de la pista:"))
     
-        while (numeroIgresado < 0 || numeroIgresado > 7200){
+        while ((numeroIgresado < 0 || numeroIgresado > 7200 )){
             alert("Duración maxima 7200 (segundos) y duración minima 0 (segundos)")
-            numeroIgresado = Number(prompt("Ingrese la duración de la pista"));
+            numeroIgresado = parseInt(prompt("Ingrese la duración de la pista"));
         }
     
     return numeroIgresado;
@@ -136,7 +89,6 @@ function validarTextoVacio(tipo){
 
 
 
-
 function mostrar() {
     console.log(discos);
     let html = "<ul>";
@@ -153,9 +105,8 @@ function mostrar() {
         html += `<strong> Autor: </strong> <li>${discos[i].Autor}</li>`;
         html += `<strong> Código: </strong> <li>${discos[i].Codigo}</li>`;
         
-        html += `<strong> Nombre Pista </strong>`;
         for (let j = 0; j < discos[i].Pistas.length; j++){
-            html += `<strong> Nombre: </strong> <li>${discos[i].Pistas[j].Nombre} \n  </li> \n <strong> Duración: </strong> <li>  ${discos[i].Pistas[j].Duracion} </li>`; 
+            html += `<strong> Nombre de la pista: </strong> <li>${discos[i].Pistas[j].Nombre} \n  </li> \n <strong id="duracion"> Duración: </strong> <li> ${discos[i].Pistas[j].Duracion} </li>`; 
         }    
 
     }
@@ -166,74 +117,5 @@ function mostrar() {
 
 };
 
+document.getElementById("enlace").style.color = "brown";
 
-
-// Nombre: 'Donda',
-// Autor: 'Kanye West',
-// Codigo: 1,
-// Pistas: [
-//             {
-//                 Nombre: 'Donda',
-//                 Autor: 'Kanye West',
-//                 Codigo: 1,
-//                 Pistas: 
-//             }
-//             {
-//                 Nombre: 'Jail',
-//                 Duracion: 457,
-//             },
-//             {
-//                 Nombre: 'God Breathed',
-//                 Duracion: 533,
-//             },
-//             {
-//                 Nombre: 'Off The Grid',
-//                 Duracion: 539,
-//             },
-//             {
-//                 Nombre: 'Hurricane',
-//                 Duracion: 403,
-//             },
-//             {
-//                 Nombre: 'Praise God',
-//                 Duracion: 346,
-//             },
-//             {
-//                 Nombre: 'Jonah',
-//                 Duracion: 315,
-//             },
-//         ],
-
-//         console.log (discos);
-//         console.table (discos);
-
-
-
-// // Función Cargar:
-// function cargar = () => {
-//     let html = '<ul>';
-//     for (const item of discos) {
-//         console.log(item)
-//         html += `
-//             <li>Kanye West</ul>
-//         `
-//     }
-
-//     html+= '</ul>';
-//     document.querySelector('#info').innerHTML = html;
-
-//     // Cositas:
-// };
-
-// // Función Mostrar:
-// const mostrar = () => {
-//     // Variable para ir armando la cadena:
-//     let html = '';
-
-//     // Cositas:
-
-//     // Si modificaste el nombre de la variable para ir armando la cadena, también hacelo acá:
-//     document.getElementById('info').innerHTML = html; // <--- ahí es acá
-// };
-
-// // Todas las funciones que necesites
