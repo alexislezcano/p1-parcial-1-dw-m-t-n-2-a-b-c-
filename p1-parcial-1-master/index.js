dd3d3c4f6f9032f068d6696a3dbcbce265a2cc16
alexislezcano <alexislezcano79@gmail.com>
2022-11-07 19:19:41 -0300

    Finalizado

--- p1-parcial-1-master/index.js
+++ p1-parcial-1-master/index.js
@@ -1,8 +1,9 @@
 'use strict';
 
 /*
- * APELLIDO, NOMBRE | APELLIDO, NOMBRE
+ * LEZCANO, ALEXIS | DE JUAN GUEVARA RAMIREZ, ZAHIR
  */
+
 
 // Discos:
 
@@ -16,10 +17,10 @@ let duracionPista;
 let pista;
 
 
+
 function agregar() {
     nombreDisco = validarTextoVacio(" del disco");
     nombreAutor = validarTextoVacio(" del autor");
-    // numeroCodigo = validarCodigo (Number(prompt("Ingrese el número de código")), discos);
     numeroCodigo = validarCodigo(discos);
 
     disco = {
@@ -42,11 +43,12 @@ function agregar() {
     }while (confirm("¿Desea ingresar otra pista?") == true);
     discos.push(disco);
 }
-//Crear la función 
-//Recorrer los códigos
-//Agregar condicional
-//El código numérico único del disco no puede ser menor a 1, ni mayor a 999.
 
+
+// Crear la función 
+// Recorrer los códigos
+// Agregar condicional
+// El código numérico único del disco no puede ser menor a 1, ni mayor a 999.
 function validarCodigo(discoVerificar){
 
     let numeroIgresado = parseInt(prompt("Ingrese el número de código:"))
@@ -60,11 +62,11 @@ function validarCodigo(discoVerificar){
 }
 
 
-//La duración de cada pista debe estar entre 0 y 7200 (segundos) inclusive.
+// La duración de cada pista debe estar entre 0 y 7200 (segundos) inclusive.
 function validarDuracion(duracionVerificar){
 
-    let numeroIgresado = parseInt(prompt("Ingrese la duración de la pista:"))
-    
+    let numeroIgresado = parseInt(prompt("Ingrese la duración de la pista:"));
+
         while ((numeroIgresado < 0 || numeroIgresado > 7200 )){
             alert("Duración maxima 7200 (segundos) y duración minima 0 (segundos)")
             numeroIgresado = parseInt(prompt("Ingrese la duración de la pista"));
@@ -73,8 +75,9 @@ function validarDuracion(duracionVerificar){
     return numeroIgresado;
 }
 
+
 // El nombre del disco, autor/banda y nombre de la pista no pueden quedar vacíos.
-//Pedir el dato, crear una condicional y saber el tamaño del texto.
+// Pedir el dato, crear una condicional y saber el tamaño del texto.
 function validarTextoVacio(tipo){
     
     let textoIngresado = prompt("Ingrese el nombre" + `${tipo}`)
@@ -88,34 +91,34 @@ function validarTextoVacio(tipo){
 }
 
 
-
-function mostrar() {
-    console.log(discos);
+// Destacar visualmente con rojo las duraciones mayores a 180
+function mostrar(){
     let html = "<ul>";
 
-    // //     for (const item of discos){
-    // //     console.log(item);
-    // // }
-
-    console.log(discos.length)
 
     for (let i = 0; i < discos.length; i++) {
-        console.log(discos[i]);
         html += `<strong> Album: </strong> <li>${discos[i].Nombre}</li>`;
         html += `<strong> Autor: </strong> <li>${discos[i].Autor}</li>`;
         html += `<strong> Código: </strong> <li>${discos[i].Codigo}</li>`;
         
+        html += `<strong> Nombre Pista </strong>`;
         for (let j = 0; j < discos[i].Pistas.length; j++){
-            html += `<strong> Nombre de la pista: </strong> <li>${discos[i].Pistas[j].Nombre} \n  </li> \n <strong id="duracion"> Duración: </strong> <li> ${discos[i].Pistas[j].Duracion} </li>`; 
-        }    
-
-    }
-
+            html += `<strong> Nombre: </strong> <li>${discos[i].Pistas[j].Nombre}</li> <strong> Duración: </strong> ${colorDuracion(discos[i].Pistas[j].Duracion)}`; 
+        }
+        }
+        
     html += "</ul>";
-
     document.querySelector("#info").innerHTML = html;
 
 };
 
-document.getElementById("enlace").style.color = "brown";
+function colorDuracion(duracion){
+
+    let retorno = `<li>${duracion}</li>`;
+
+    if(duracion >= 180){
+        retorno = `<li style="color: red;"> ${duracion}</li>`
+    }
+    return retorno;
+};
 
